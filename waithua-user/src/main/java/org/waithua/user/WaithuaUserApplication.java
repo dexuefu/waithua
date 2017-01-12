a524f2a10622201b4e7353509176c828918a5b84
dexuefu <fdexue@gmail.com>
2017-01-12 16:09:34 +0800

    add user

---
+++ waithua-user/src/main/java/org/waithua/user/WaithuaUserApplication.java
@@ -0,0 +1,7 @@
+package org.waithua.user;
+
+/**
+ * Created by jch on 17/1/12.
+ */
+public class WaithuaUserApplication {
+}
---
+++ waithua-web/src/main/java/org/waithua/web/entity/User.java
@@ -0,0 +1,15 @@
+package org.waithua.web.entity;
+
+import lombok.Data;
+
+/**
+ * Created by jch on 17/1/12.
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+    private String sex;
+
+}
