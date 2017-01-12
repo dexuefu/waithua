e61c61555b67c915434f7d6261e021c71a7e057a
dexuefu <fdexue@gmail.com>
2017-01-12 15:45:06 +0800

    change port

--- waithua-web/src/main/java/org/waithua/web/WaithuaWebApplication.java
+++ waithua-web/src/main/java/org/waithua/web/WaithuaWebApplication.java
@@ -1,8 +1,14 @@
 package org.waithua.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by jch on 17/1/12.
  */
+@RestController
 @SpringBootApplication
 public class WaithuaWebApplication {
     @RequestMapping("/")
