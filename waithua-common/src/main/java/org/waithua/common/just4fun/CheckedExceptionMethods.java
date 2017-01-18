f93057ab9e593b97ca5c6b2074f92829be8bc4ec
dexuefu <fdexue@gmail.com>
2017-01-18 22:06:29 +0800

    threadtest

---
+++ waithua-common/src/main/java/org/waithua/common/just4fun/CheckedExceptionMethods.java
@@ -0,0 +1,7 @@
+package org.waithua.common.just4fun;
+
+/**
+ * Created by jch on 17/1/16.
+ */
+public class CheckedExceptionMethods {
+}
---
+++ waithua-common/src/main/java/org/waithua/common/just4fun/threadandrunnable/ThreadComunication.java
@@ -0,0 +1,7 @@
+package org.waithua.common.just4fun.threadandrunnable;
+
+/**
+ * Created by jch on 17/1/18.
+ */
+public class ThreadComunication {
+}
